22593029016ae946d8c971a4f6033dfb3b358f95
Chong Yao Jun <82208147+Im-Not-God@users.noreply.github.com>
2022-08-20 17:56:46 +0800

    Add files via upload

--- constants/icons.js
+++ constants/icons.js
@@ -1,25 +1,55 @@
-const airplay = require("../assets/icons/airplay.png");
-const home = require("../assets/icons/home.png");
+
+const cinema_logo=require("../assets/icons/cinema_logo.png")
+const cinema_logo_2=require("../assets/icons/cinema_logo_2.png")
+const cinema_logo_3=require("../assets/icons/cinema_logo_3.png")
+const cinemaLogo_horizontal=require("../assets/icons/cinemaLogo_horizontal.png")
+const cinemaLogo_vertical=require("../assets/icons/cinemaLogo_vertical.png")
 const left_arrow = require("../assets/icons/left-arrow.png");
 const play = require("../assets/icons/play.png");
-const play_button = require("../assets/icons/play-button.png");
-const profile = require("../assets/icons/profile.png");
 const right_arrow = require("../assets/icons/right-arrow.png");
 const search = require("../assets/icons/search.png");
 const star = require("../assets/icons/star.png");
 const fire = require("../assets/icons/fire.png");
 const upload = require("../assets/icons/upload.png");
 
+import React from "react"
+import Ionicons from "react-native-vector-icons/Ionicons";
+import AntDesign from "react-native-vector-icons/AntDesign";
+import FontAwesome from "react-native-vector-icons/FontAwesome";
+
+const ionicons = ({ name, size, color, style, onPress }) => {
+    return(
+        <Ionicons name={name} size={size} color={color} style={style} onPress={onPress}/>
+    );
+}
+
+const antDesign = ({ name, size, color, style, onPress}) => {
+    return(
+        <AntDesign name={name} size={size} color={color} style={style} onPress={{onPress}}/>
+    );
+}
+
+const fontAwesome = ({ name, size, color, style, onPress}) => {
+    return(
+        <FontAwesome name={name} size={size} color={color} style={style} onPress={{onPress}}/>
+    );
+}
+
 export default {
-    airplay,
-    home,
+    cinema_logo,
+    cinema_logo_2,
+    cinema_logo_3,
+    cinemaLogo_horizontal,
+    cinemaLogo_vertical,
     left_arrow,
     play,
-    play_button,
-    profile,
     right_arrow,
     search,
     star,
     fire,
-    upload
+    upload,
+    ionicons,
+    antDesign,
+    fontAwesome
+    
 }
--- constants/images.js
+++ constants/images.js
@@ -1,5 +1,5 @@
 const profile_photo = require("../assets/images/dummy_profile/profile_photo.png");
 
 export default {
-    profile_photo,
+    profile_photo
 }
