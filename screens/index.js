98a052540d9a38e12730623eea084965241aafa1
Chong Yao Jun <82208147+Im-Not-God@users.noreply.github.com>
2022-08-24 03:15:09 +0800

    add signup

--- screens/index.js
+++ screens/index.js
@@ -2,10 +2,12 @@ import Home from "./Home"
 import MovieDetail from "./MovieDetail"
 import SignIn from "./SignIn"
 import GetStarted from "./GetStarted";
+import SignUp from "./SignUp"
 
 export {
     Home,
     MovieDetail,
     SignIn,
-    GetStarted
+    GetStarted,
+    SignUp
 };
