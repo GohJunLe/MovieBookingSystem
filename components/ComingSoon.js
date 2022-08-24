60510a83450d1363fc2522a3ace54d0fe9e9aa95
DHST7988 <109194171+DHST7988@users.noreply.github.com>
2022-08-24 14:58:46 +0800

    New components create for search function

---
+++ components/ComingSoon.js
@@ -0,0 +1,89 @@
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  FlatList,
+  SafeAreaView,
+  Keyboard,
+  Image,
+  TouchableOpacity,
+} from "react-native";
+import {
+  COLORS,
+  SIZES,
+  FONTS,
+} from "../constants";
+
+//To get the movie details that want to display out
+const Movie = ({ id, poster_path, title, navigation}) => (
+  <View style = {{height: 250, width: 210, paddingLeft: 10}}>
+    <TouchableOpacity
+                  onPress={() =>
+                    navigation.navigate("MovieDetail", { selectedMovie: id })
+                  }
+                >
+                <Image
+                      source={{uri: "https://image.tmdb.org/t/p/w500"+poster_path,}}
+                      resizeMode="cover"
+                      style={{
+                        width: SIZES.width / 3,
+                        height: SIZES.width / 3 + 60,
+                        borderRadius: 20,
+                      }}
+                    />
+                    <Text
+                      style={{
+                        color: COLORS.white,
+                        marginTop: SIZES.base,
+                        width: 150,
+                        ...FONTS.h4,
+                      }}
+                    >
+                      {title}
+                    </Text>
+                </TouchableOpacity>
+                </View>
+);
+
+// Search filter
+const  ComingSoon = ({ searchPhrase, setClicked, data, navigation}) => {
+  const renderItem = ({item}) => {
+    //Show all when no search phrase
+    if (searchPhrase === "") {
+      return <Movie id={item.id} poster_path={item.poster_path} title={item.title} navigation={navigation} />;
+    }
+    //return the movie with same phrase in the search phrase
+    if (item.title.toUpperCase().includes(searchPhrase.toUpperCase().trim().replace(/\s/g, ""))) {
+      return <Movie id={item.id} poster_path={item.poster_path} title={item.title} navigation={navigation} />;
+    }
+  };
+
+  return (
+    <SafeAreaView style={styles.list__container}>
+      <View
+        onStartShouldSetResponder={() => {
+          setClicked(false);
+        }}
+      >  
+        <FlatList 
+          key = {"_"}
+          numColumns = {2}
+          data={data}
+          renderItem={renderItem}
+          keyExtractor={(item) => item.id}
+        />
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default ComingSoon;
+
+const styles = StyleSheet.create({
+  list__container: {
+    margin: 10,
+    height: "70%",
+    width: "100%",
+  },
+});
