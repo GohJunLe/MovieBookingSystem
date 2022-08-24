2310861ee62ee4d0991523073a5b7a01bbb8d5e3
DHST7988 <109194171+DHST7988@users.noreply.github.com>
2022-08-24 15:00:23 +0800

    Added the newly added components

--- components/index.js
+++ components/index.js
@@ -3,5 +3,8 @@ import Profiles from "./Profiles";
 import ProgressBar from "./ProgressBar";
 import AppIcon from "./AppIcon";
 import Loading from "./Loading"
+import ComingSoon from "./ComingSoon";
+import NowPlaying from "./NowPlaying";
+import SearchBar from "./SearchBar"
 
-export { TabIcon, Profiles, ProgressBar ,AppIcon, Loading};
+export { TabIcon, Profiles, ProgressBar ,AppIcon, Loading, ComingSoon, NowPlaying, SearchBar};
