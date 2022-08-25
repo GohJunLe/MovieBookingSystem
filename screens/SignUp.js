491bafad139cebe59e91444b0ddf75c6466efe7d
GohJunLe <108907711+GohJunLe@users.noreply.github.com>
2022-08-25 20:59:28 +0800

    Update SignUp.js

--- screens/SignUp.js
+++ screens/SignUp.js
@@ -17,12 +17,31 @@ import { AppIcon } from "../components";
 import signUpAuth from "../accountAuth/signUp_auth";
 import DeviceStorage from "../database/deviceStorage";
 import { HelperText, TextInput, IconButton } from "react-native-paper";
+import { TrendingUp } from "react-native-feather";
+let SQLite = require('react-native-sqlite-storage');
+var db = SQLite.openDatabase({name: 'MovieBookingSystem'}, null, SQLite.CREATE_IF_NECESSARY,);
 
 const SignUp = ({ navigation }) => {
   const [email, setEmail] = React.useState("");
   const [password, setPassword] = React.useState("");
 
   const [passwordVisible, setPasswordVisible] = React.useState(false);
+  const [exist, setExist] = React.useState(false);
+
+  React.useEffect(()=>{
+    setExist(false)
+      db.transaction(tx => {
+          tx.executeSql(
+              'SELECT * FROM profile WHERE email = ?',
+              [email],
+              (tx, results) => {
+                if (results.rows.length) {
+                  setExist(true)
+                }
+              }, 
+          )
+      })
+  },[email])
 
   function backBtn() {
     return (
@@ -30,6 +49,7 @@ const SignUp = ({ navigation }) => {
         style={{
           marginTop: Platform.OS === "ios" ? 40 : 20,
           paddingHorizontal: SIZES.padding,
+          position: "absolute"
         }}
       >
         <TouchableOpacity
@@ -171,8 +191,48 @@ const SignUp = ({ navigation }) => {
 
   function result() {
     if (signUpAuth.validation(email, password)) {
-      DeviceStorage.saveData("signIn2", "false");
-      navigation.navigate("SignIn");
+      if(exist){
+        Alert.alert(
+          "Email exist",
+          "This email already exists, please go to the login screen to log in to your account.",
+          [
+            {
+              text: "Cancel",
+              style: "cancel",
+            },
+          ]
+        );
+
+      }else {
+        DeviceStorage.saveData("signIn2", "false");
+        db.transaction(tx => {
+          tx.executeSql(
+            'INSERT INTO profile(email,password) VALUES(?,?)',
+            [email, password], 
+            (tx, results) => {
+              if (results.rowsAffected > 0) {
+                console.log('data inserted successfully');
+                db.transaction(tx => {
+                  tx.executeSql(
+                    'SELECT * FROM profile WHERE email = ?',
+                    [email],
+                    (tx, results) => {
+                      if (results.rows.length) {
+                        console.log(results.rows.raw())
+                      }
+                    },
+                  );
+                });
+              } else {
+                console.log('error in inserting data');
+              }
+            }
+        )
+        })
+        navigation.navigate("SignIn");
+      }
+      
+      
     } else {
       Alert.alert(
         "Form NOT completed / INVALID email or password",
@@ -201,7 +261,6 @@ const SignUp = ({ navigation }) => {
 
   return (
     <ScrollView style={{ backgroundColor: COLORS.black }}>
-      {backBtn()}
       <View style={styles.container}>
         <View style={{ alignItems: "center" }}>
           {signUpTitle()}
@@ -221,6 +280,7 @@ const SignUp = ({ navigation }) => {
           {signIn()}
         </View>
       </View>
+      {backBtn()}
     </ScrollView>
   );
 };
@@ -231,6 +291,7 @@ const styles = StyleSheet.create({
     height: "100%",
   },
   title: {
+    marginTop: 100,
     color: COLORS.white,
     ...FONTS.h1,
   },
